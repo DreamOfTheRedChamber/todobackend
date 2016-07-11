29670352d92696aed39263913ae0b0cfe735d4e4
FreemanZhang <sjzhang1901@yahoo.com>
2016-07-10 17:30:44 -0700

    create release environment

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -23,7 +23,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
         'USER': os.environ.get('MYSQL_USER','root'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD','root'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
         'HOST': os.environ.get('MYSQL_HOST','localhost'),
         'PORT': os.environ.get('MYSQL_PORT','3306'),
     }
